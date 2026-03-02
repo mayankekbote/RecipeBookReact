8649ad5682ab9d73e4842d489b35ed9b7163143e
Mayank Ekbote <mayankekbote12@gmail.com>
2026-03-02 12:26:51 +0530

    fixed styling issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div className='bg-gradient-to-br from-gray-900 via-gray-800 to-pink-900 py-10 px-[20%] w-screen min-h-screen text-white font-thin bg-gray-700'>
-      <Navbar />
-      <Mainroutes />
+    <div className="min-h-screen text-white font-thin">
+      
+      <div className="max-w-6xl mx-auto px-6 py-10">
+        <Navbar />
+        <Mainroutes />
+      </div>
+
     </div>
   )
 }
